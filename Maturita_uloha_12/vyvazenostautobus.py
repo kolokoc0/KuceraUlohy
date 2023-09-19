e1b272f434666e326820909cc00f61734621c184
kolokoc0 <kristiansvrlo@gmail.com>
2023-09-19 22:47:35 +0200

    ulohicky

---
+++ Maturita_uloha_12/vyvazenostautobus.py
@@ -0,0 +1,25 @@
+from re import split
+subor = open("bus_vytazenost.txt","r")
+
+buses = subor.readlines()
+num_buses = len(buses)-1
+over_names = []
+over_num = []
+names = []
+limit = int(buses[0].strip())
+current_num = 0
+
+for line in buses[1::]:
+    line= line.strip()
+    inside, outside, name = split('\s',line,2)
+    names.append(name)
+    current_num = current_num + int(inside) - int(outside)
+    if current_num>limit:
+        over_names.append(name)
+        over_num.append(current_num)
+
+print(f'Zastavok je: {num_buses}')
+print('Zastavky: ' + ", ".join(names))
+print(f'Po nastupeni limit bol prekroceny na zastavkach: {", ".join(over_names)}')
+print(f'Najviac ludi nad ramec kapacity bolo: {max(over_num)} na zastavke: {over_names[over_num.index(max(over_num))]}')
+
